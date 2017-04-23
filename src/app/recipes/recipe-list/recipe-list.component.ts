4b26469c0961b781391b7a928f280649b1b0a0d6
Francisco Ricardo <francisco.ricardot@gmail.com>
2017-04-23 01:52:40 -0300

    Define the recipes attribute in RecipeListComponent

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,3 +1,4 @@
+import { Recipe } from './../recipe.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe',
+      'This is simply a test',
+      'https://www.google.com.br/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwjt7JmM4bnTAhUJvJAKHfihAOwQjRwIBw&url=https%3A%2F%2Fwww.nps.gov%2Fsubjects%2Fcamping%2Frecipes.htm&psig=AFQjCNF0uvQqi997SNUCOE82RO6tJjRFzw&ust=1493008721201351')
+  ];
 
-  constructor() { }
 
-  ngOnInit() {
-  }
+  constructor() {}
+
+  ngOnInit() {}
 
 }
