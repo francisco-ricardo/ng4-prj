bbe74fb91a663b194ddacaa26ade27d93213d73b
Francisco Ricardo <francisco.ricardot@gmail.com>
2017-04-24 00:43:48 -0300

    Define the recipe list component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,10 @@
-import { Recipe } from './../recipe.model';
-import { Component, OnInit } from '@angular/core';
+import {
+  Recipe
+} from './../recipe.model';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
 
 @Component({
   selector: 'app-recipe-list',
@@ -10,7 +15,11 @@ export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe('A Test Recipe',
       'This is simply a test',
-      'https://www.google.com.br/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwjt7JmM4bnTAhUJvJAKHfihAOwQjRwIBw&url=https%3A%2F%2Fwww.nps.gov%2Fsubjects%2Fcamping%2Frecipes.htm&psig=AFQjCNF0uvQqi997SNUCOE82RO6tJjRFzw&ust=1493008721201351')
+      'https://www.google.com.br/imgres?imgurl=http%3A%2F%2Ffoodtalk4you.com%2Fwp-content%2Fuploads%2F2017%2F01%2FRecipes-Banner.jpg&imgrefurl=http%3A%2F%2Ffoodtalk4you.com%2Frecipes%2F&docid=f1F8lzl-Q0949M&tbnid=AVbvsS4oLmjvpM%3A&vet=10ahUKEwiJ3u6qk7zTAhVLhJAKHRNhDqEQMwg5KAEwAQ..i&w=1024&h=576&bih=678&biw=1366&q=recipes&ved=0ahUKEwiJ3u6qk7zTAhVLhJAKHRNhDqEQMwg5KAEwAQ&iact=mrc&uact=8'
+      ),
+    new Recipe('A Test Recipe',
+      'This is simply a test',
+      'https://www.google.com.br/imgres?imgurl=http%3A%2F%2Ffoodtalk4you.com%2Fwp-content%2Fuploads%2F2017%2F01%2FRecipes-Banner.jpg&imgrefurl=http%3A%2F%2Ffoodtalk4you.com%2Frecipes%2F&docid=f1F8lzl-Q0949M&tbnid=AVbvsS4oLmjvpM%3A&vet=10ahUKEwiJ3u6qk7zTAhVLhJAKHRNhDqEQMwg5KAEwAQ..i&w=1024&h=576&bih=678&biw=1366&q=recipes&ved=0ahUKEwiJ3u6qk7zTAhVLhJAKHRNhDqEQMwg5KAEwAQ&iact=mrc&uact=8')
   ];
 
 
