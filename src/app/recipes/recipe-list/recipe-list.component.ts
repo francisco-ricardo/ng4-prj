23615b598a4c8d3398e619f0eb2a1b4e211e2df9
Francisco Ricardo <francisco.ricardot@gmail.com>
2017-06-03 01:05:17 -0300

    Ajuste o recipe-list component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -15,11 +15,11 @@ export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe('A Test Recipe',
       'This is simply a test',
-      'https://www.google.com.br/imgres?imgurl=http%3A%2F%2Ffoodtalk4you.com%2Fwp-content%2Fuploads%2F2017%2F01%2FRecipes-Banner.jpg&imgrefurl=http%3A%2F%2Ffoodtalk4you.com%2Frecipes%2F&docid=f1F8lzl-Q0949M&tbnid=AVbvsS4oLmjvpM%3A&vet=10ahUKEwiJ3u6qk7zTAhVLhJAKHRNhDqEQMwg5KAEwAQ..i&w=1024&h=576&bih=678&biw=1366&q=recipes&ved=0ahUKEwiJ3u6qk7zTAhVLhJAKHRNhDqEQMwg5KAEwAQ&iact=mrc&uact=8'
-      ),
+      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'
+    ),
     new Recipe('A Test Recipe',
       'This is simply a test',
-      'https://www.google.com.br/imgres?imgurl=http%3A%2F%2Ffoodtalk4you.com%2Fwp-content%2Fuploads%2F2017%2F01%2FRecipes-Banner.jpg&imgrefurl=http%3A%2F%2Ffoodtalk4you.com%2Frecipes%2F&docid=f1F8lzl-Q0949M&tbnid=AVbvsS4oLmjvpM%3A&vet=10ahUKEwiJ3u6qk7zTAhVLhJAKHRNhDqEQMwg5KAEwAQ..i&w=1024&h=576&bih=678&biw=1366&q=recipes&ved=0ahUKEwiJ3u6qk7zTAhVLhJAKHRNhDqEQMwg5KAEwAQ&iact=mrc&uact=8')
+      'https://images-gmi-pmc.edge-generalmills.com/eb52c020-c145-440c-8445-911f133c0096.jpg')
   ];
 
 
